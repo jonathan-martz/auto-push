f11b4dcc4e3b42a19ff6416a5708150b9dafa059
jonathan-martz <github@jmartz.de>
2020-01-01 20:50:58 +0100

    on start git pull, add check for uptime and change detection

--- run.sh
+++ run.sh
@@ -2,15 +2,29 @@
 
 for d in */ ; do
     if [ -d "$d/.git" ]; then
-      echo "$d git folder"
       cd "$d" && git push origin
       cd ..
     else
       cd "$d"
       for dd in */ ; do
           if [ -d "$dd/.git" ]; then
-            echo "$dd"
-            cd "$dd" && git push origin
+            cd "$dd"
+
+            upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
+            upMins=$((${upSeconds} / 60))
+
+            if [ "${upMins}" -lt "5" ]
+            then
+                git pull --dry-run > /dev/null 2>&1 | grep -q -v 'Already up-to-date.' && changed=1
+
+                if [ "$changed" != 1 ] ; then
+                    git pull origin
+                else
+                    notify-send "Repo: $d$dd" "Changes detect, you need to update it manualy."
+                fi
+            fi
+
+            git push origin
             cd ..
           fi
       done
