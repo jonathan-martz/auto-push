0b8a55f194a7ec0130de8d2f8ac8e62968ddfa5a
Jonathan Martz <git@jmartz.de>
2020-03-22 12:10:29 +0100

    remove notify

--- run.sh
+++ run.sh
@@ -10,19 +10,11 @@ for d in */ ; do
           if [ -d "$dd/.git" ]; then
             cd "$dd"
 
-            upSeconds="$(cat /proc/uptime | grep -o '^[0-9]\+')"
-            upMins=$((${upSeconds} / 60))
+              git pull --dry-run > /dev/null 2>&1 | grep -q -v 'Already up-to-date.' && changed=1
 
-            if [ "${upMins}" -lt "5" ]
-            then
-                git pull --dry-run > /dev/null 2>&1 | grep -q -v 'Already up-to-date.' && changed=1
-
-                if [ "$changed" != 1 ] ; then
-                    git pull origin
-                else
-                    notify-send "Repo: $d$dd" "Changes detect, you need to update it manualy."
-                fi
-            fi
+              if [ "$changed" != 1 ] ; then
+                  git pull origin
+              fi
 
             git push origin
             cd ..
